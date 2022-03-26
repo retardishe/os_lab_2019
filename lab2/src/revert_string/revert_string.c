ff7b83888156bcac957f70fc8f80a1e8a047a5df
retardishe <az993@bk.ru>
2022-03-26 12:12:08 +0000

    add some lab2 files

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
+#include <string.h>
+#include <stddef.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	char temp;
+	int len = strlen(str);
+	for (size_t i = 0; i< len/2; i++) {
+		temp = str[i];
+		str[i] = str[len-i-1];
+		str[len-i-1] = temp;
+	}
+	return;
 }
 
